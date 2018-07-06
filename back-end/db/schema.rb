49e2c7a3916b890cde10c987b4fdb1e3b373d063
github username <github email address>
2018-07-06 11:17:52 -0400

    add heroku changes

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180510130317) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lyrics", force: :cascade do |t|
     t.integer "song_id"
     t.float "start"
